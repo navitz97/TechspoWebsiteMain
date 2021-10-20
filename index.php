a7fc72c5455c7e08043a2cb7dc22168627047d3e
Stefan van Deventer <611554@student.inholland.nl>
2021-10-20 17:29:14 +0200

    Changed nav on index

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Styles.css?v=<?php echo time(); ?>">
+    <title>Welcome</title>
+</head>
+
+<body>
+    <!-- Website logo -->
+    <div class="logo">
+        <img src="images/Logo_2.png" alt="logo">
+    </div>
+
+    <!-- Navigation Bar -->
+    <div class="navbar">
+        <a href="index.html" class="active">Home</a>
+        <a href="About.html">About</a>
+        <a href="Store.html">Shop</a>
+        <a href="Contact.html">Contact</a>
+        <a href="Members.html">Members</a>
+    </div>
+
+    <!-- <div class="rec"></div> -->
+    <div class="container">
+        <img src="Images/Welcome_image.png" width="100%" />
+        <div class="text">
+            <h2>A Big Tech Collabortion</h2><br>
+            Be prepared to be inspired, amazed and educated on the evolution of technology that
+            will impact your business growth.
+        </div>
+    </div>
+
+</body>
+
+</html>
