728d8a7d55550560566fd4ed099138949f41b62d
Stefan van Deventer <611554@student.inholland.nl>
2021-10-25 19:00:29 +0200

    About page

--- About.php
+++ About.php
@@ -10,22 +10,30 @@
 </head>
 
 <body>
-    <!-- Website logo -->
-    <div class="logo">
-        <img src="images/Logo_2.png" alt="logo">
+    <!-- Navigation -->
+    <div class="navigation">
+        <!-- Website logo -->
+        <div class="logo">
+            <img src="images/logo.svg" alt="logo">
+        </div>
+
+        <!-- Navigation Bar -->
+        <div class="navbar">
+            <a href="index.php">Home</a>
+            <a href="About.php" class="active">About</a>
+            <a href="Store.php">Shop</a>
+            <a href="Contact.php">Contact</a>
+            <a href="Members.php">Members</a>
+        </div>
     </div>
 
-    <!-- Navigation Bar -->
-    <div class="navbar">
-        <a href="index.php">Home</a>
-        <a href="About.php" class="active">About</a>
-        <a href="Store.php">Shop</a>
-        <a href="Contact.php">Contact</a>
-        <a href="Members.php">Members</a>
-    </div>
-    <div class="container">
+    <div class="about-hero">
         <img src="Images/color_letter.png" width="100%">
-        <div class="text2">
+
+        <div class="about-hero_content">
+            <h1>About Techspo</h1>
+        </div>
+        <!-- <div class="text2">
             <h2>The Expo of Technologies</h2><br>
             Techspo is a two date technology expo where some of the best developers, brands, marketers,
             technology providers, designers and evangelists showcase the next generation of advances
@@ -40,9 +48,26 @@
             2021 Digital Marketing, Media and Advertising Conference. If the conference is where
             the learning, theory and inspiration happens, then the TECHSPO floor is where the testing,
             Network and product interaction takes place.
-        </div>
+        </div> -->
     </div>
 
+    <div class="about-content">
+        <h2>The Expo of Technologies</h2><br>
+        <p style="text-align: left;">
+            Techspo is a two day technology expo where some of the best developers, brands, markerters,
+            technology providers, designers and evangelists showcase the next generation of advances in
+            technology, including internet, mobile, Ad Tech , Martech and SaaS technologies. <br><br>
+            Showcasing the next generation of technology & innovation; Internet, Mobile, AdTech, MarTech &
+            SaaS Technology, Exhibitors have the opportunity to show off their companies to consumers, the
+            highest caliber investors, hordes of press, the most sought after talent, and the greatest pool of tech
+            enthusiasts looking to celebrate emerging products. Be prepared to be inspired, amazed and educated
+            on how these evolving technologies will impact your business for greater growth.<br><br>
+            As part of TECHSPO Johannesburg is a limited attendance paid event, DigiMarCon South Africa 2021
+            Digital Marketing, Media and Advertising Conference. If the conference is where the learning, theory
+            and inspiration happens.
+        </p>
+
+    </div>
 </body>
 
 </html>
