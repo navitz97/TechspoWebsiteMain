c375c6698bc38305d860b93224ee9d0d4f59f52b
Stefan van Deventer <611554@student.inholland.nl>
2021-11-03 16:38:21 +0200

    store and created config file

--- Store.php
+++ Store.php
@@ -1,3 +1,61 @@
+<?php
+    session_start();
+    include('config.php');
+    $status = "";
+
+    if(isset($_POST['code']) && $_POST['code'] != "")
+    {
+        $code = $_POST['code'];
+        $result = mysqli_query(
+            $Link,
+            "SELECT * FROM courses WHERE code = '$code'"
+        );
+
+        $row = mysqli_fetch_assoc($result);
+
+        $name = $row['name'];
+        $code = $row['code'];
+        $price = $row['price'];
+        $image = $row['image'];
+
+        $wishArray = array(
+            $code => array(
+                'name' => $name,
+                'code' => $code,
+                'price' => $price,
+                'quantity' => 1,
+                'image' => $image
+            )
+        );
+
+        if(empty($_SESSION["cart"]))
+        {
+            $_SESSION["cart"] = $wishArray;
+            $status = "Product added to cart!";
+        }
+
+        else
+        {
+            $wish_keys = array_keys($_SESSION["cart"]);
+
+            if(in_array($code,$wish_keys))
+            {
+                $status = "Product is already on your list!";
+            }
+
+            else
+            {
+                $_SESSION["cart"] = array_merge(
+                    $_SESSION["cart"],
+                    $wishArray
+                );
+
+                $status = "Course is already in your cart!";
+            }
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,6 +80,12 @@
         <a href="Store.php" class="active">Shop</a>
         <a href="Contact.php">Contact</a>
         <a href="Members.php">Members</a>
+    </div>
+
+    <div class="store">
+        <div class="products">
+
+        </div>
     </div>
 </body>
 
